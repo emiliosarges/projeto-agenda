749832700fbcebd1fb56544d6a7e85e6988826ae
Emílio Silva <emiliosarges@gmail.com>
2025-04-09 23:25:27 -0300

    Conexão com o DB
    Conexão com o BD. Configuração da classe JavaBeans, da classe DAO e do servlet Controller para conexão ao Banco de Dados.

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,35 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/** Módulo de conexão **/
+	// Parâmetros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver"; //funciona somente se o driver estiver na pasta lib
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "root";
+	private String password = "";
+	// Método de conexão
+	private Connection conectar() { //ctrl + shift + O para importar uma classe no Eclipse
+		Connection con = null;
+		try {
+			Class.forName(driver); //Busca informação da variável "driver" criado nos parâmetros. 
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	// teste de conexão
+	public void testeConexao() {
+		try {
+			Connection con = conectar ();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
